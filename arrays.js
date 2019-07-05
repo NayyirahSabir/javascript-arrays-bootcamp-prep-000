cad742f024672bb8868a0e1b7ce0541310bfceca
nsabir <awesomeartist15@gmail.com>
2019-07-05 23:01:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,9 +5,51 @@ var chocolateBars = [
   'skittles'
   ]
   
-  var addElementToBeginningOfArray = ['array', 'element']
-  addElementToBeginningOfArray.unshift("element")
-  var addElementToBeginningOfArray = ['element', ...addElementToBeginningOfArray]
   
-  var destructivelyAddElementToBeginningOfArray = ['array', 'element']
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array]
+}
+
+function addElementToEndOfArray(array, element){
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
+
+function accessElementInArray(array, index){
+  console.log(array[index])
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array){
+  array.slice()
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  array.slice(array.length - 1)
+  return array
+}
+
+
+  
+  
   
