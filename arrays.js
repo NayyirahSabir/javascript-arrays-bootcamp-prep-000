c72956f26a81f9f8622b2bce2fd44d8f8a92bf4a
nsabir <awesomeartist15@gmail.com>
2019-07-06 00:40:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -44,7 +44,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  array.slice(array.length -1)
+  array.slice(1, array.length -1)
 }
 
 
